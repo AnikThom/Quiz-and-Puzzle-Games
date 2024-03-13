304611f4d7ea9f51b6a03546a49b0b7f3bca9fda
Anice Thompson <athompson186850@troy.edu>
2024-03-13 18:36:39 -0500

    Update quizgame.js

--- Game Pages/Quiz Game/quizgame.js
+++ Game Pages/Quiz Game/quizgame.js
@@ -1,7 +1,28 @@
 let newImage = new Image();
-newImage.src = 'path/to/your/image.jpg';
+newImage.src = 'Game Pages/Quiz Game/USA Map.png';
 
 var questions = [
-  "USA" <img src = "USA Map.png" alt= "USA Map">,
-  "Who won the FIFA Women's World Cup in 2019?",
+  "USA"
 ];
+questions.push(newImage);
+
+function getRandomQuestion() {
+  var randomIndex = Math.floor(Math.random() * questions.length);
+  return questions[randomIndex];
+  }
+
+function displayQuiz(maxQuestions) {
+  var quiz = document.getElementById("quiz");
+  var numQuestionsDisplayed = 0;
+    
+  while (numQuestionsDisplayed < maxQuestions) {
+    var question = getRandomQuestion();
+          
+    var questionDiv = document.createElement("div");
+    questionDiv.innerHTML = question;
+    quiz.appendChild(questionDiv);
+      
+    numQuestionsDisplayed++;
+  }
+}
+displayQuiz(10);
