a6ecc0746336cfce65d7794fba7b95ec4cafb222
Anice Thompson <athompson186850@troy.edu>
2024-03-13 18:28:38 -0500

    Update quizgame.js

--- Game Pages/Quiz Game/quizgame.js
+++ Game Pages/Quiz Game/quizgame.js
@@ -1,3 +1,6 @@
+let newImage = new Image();
+newImage.src = 'path/to/your/image.jpg';
+
 var questions = [
   "USA" <img src = "USA Map.png" alt= "USA Map">,
   "Who won the FIFA Women's World Cup in 2019?",
