98fe20c587b51a2506869fc7f8e3a95d15f5a89b
Anice Thompson <athompson186850@troy.edu>
2024-03-13 20:19:52 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/statequiz.js
+++ Game Pages/Quiz Game/statequiz.js
@@ -1,13 +1,12 @@
-const images = [
-        'USA Map.png',
-        'Rainbow.png',
-        'Dictionary.png'
-];
-     
-const container = document.getElementById('image-container');
-     
-for (let i = 0; i < images.length; i++) {
-        const img = document.createElement('img');
-        img.src = images[i];
-         container.appendChild(img);
-}
+var pos = 0,
+  test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+
+var questions = [{ //1
+    question: "What is Dazai's power from Bungo Stray Dogs?",
+    a: "No Longer Human",
+    b: "For the Tainted Sorrow",
+    c: "Rashomon",
+    d: "Unbreakable",
+    answer: "A",
+    img: "USA Map.png"
+  }];
