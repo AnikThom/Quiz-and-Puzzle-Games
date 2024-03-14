84a9bc4910645ff58282bd0e6cb665dc1289e1f4
Anice Thompson <athompson186850@troy.edu>
2024-03-13 20:36:08 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/statequiz.js
+++ Game Pages/Quiz Game/statequiz.js
@@ -58,15 +58,3 @@ function displayQuiz(maxQuestions) {
 
 displayQuiz(10);
 
-var pos = 0,
-  test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
-
-var questions = [{
-    question: "What is Dazai's power from Bungo Stray Dogs?",
-    a: "No Longer Human",
-    b: "For the Tainted Sorrow",
-    c: "Rashomon",
-    d: "Unbreakable",
-    answer: "A",
-    img: "USA Map.png"
-  }];
