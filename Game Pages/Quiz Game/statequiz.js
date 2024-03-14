b981dce94c914b6967440f2caae3d6b204828fe6
Anice Thompson <athompson186850@troy.edu>
2024-03-13 20:06:31 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/statequiz.js
+++ Game Pages/Quiz Game/statequiz.js
@@ -1,13 +1,14 @@
-        const images = [
-'USA Map.png',
-'Rainbow.png',
-'Dictionary.png'
-        ];
+const images = [
+        'USA Map.png'
+        title: 'What State is This?',
+        'Rainbow.png',
+        'Dictionary.png'
+];
      
-        const container = document.getElementById('image-container');
+const container = document.getElementById('image-container');
      
-        for (let i = 0; i < images.length; i++) {
-            const img = document.createElement('img');
-            img.src = images[i];
-            container.appendChild(img);
-        }
+for (let i = 0; i < images.length; i++) {
+        const img = document.createElement('img');
+        img.src = images[i];
+         container.appendChild(img);
+}
