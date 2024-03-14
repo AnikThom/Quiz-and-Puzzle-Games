63964eb2b5dc275c18f77aaaa9e9fc660e4384f4
Anice Thompson <athompson186850@troy.edu>
2024-03-13 19:39:21 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/statequiz.js
+++ Game Pages/Quiz Game/statequiz.js
@@ -1,31 +1,11 @@
-var imgArray = new Array();
+        const images = [
 
-imgArray[0] = new Image();
-imgArray[0].src = 'Game Pages/Quiz Game/USA Map.png';
-document.getElementById("quiz").src = imgArray.src;
-
-var questions = [
-  "USA"
-];
-questions.push(imgArray);
-
-function getRandomQuestion() {
-  var randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
-  }
-
-function displayQuiz(maxQuestions) {
-  var quiz = document.getElementById("quiz");
-  var numQuestionsDisplayed = 0;
-    
-  while (numQuestionsDisplayed < maxQuestions) {
-    var question = getRandomQuestion();
-          
-    var questionDiv = document.createElement("div");
-    questionDiv.innerHTML = question;
-    quiz.appendChild(questionDiv);
-      
-    numQuestionsDisplayed++;
-  }
-}
-displayQuiz(10);
+        ];
+     
+        const container = document.getElementById('image-container');
+     
+        for (let i = 0; i < images.length; i++) {
+            const img = document.createElement('img');
+            img.src = images[i];
+            container.appendChild(img);
+        }
