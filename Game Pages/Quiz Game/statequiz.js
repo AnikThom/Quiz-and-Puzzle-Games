333f5a9a07072891108a521095e5b1fd64d63619
Anice Thompson <athompson186850@troy.edu>
2024-03-13 20:45:04 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/statequiz.js
+++ Game Pages/Quiz Game/statequiz.js
@@ -1,60 +1,93 @@
-var questions = [
-  {
-  question: "What state is this?",
-  a: "No Longer Human",
-  b: "For the Tainted Sorrow",
-  c: "Rashomon",
-  d: "Unbreakable",
-  answer: "A",
-  img: "USA Map.png"
+(function(){
+  function buildQuiz(){
+    const output = [];
+
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+
+        const answers = [];
+
+        for(letter in currentQuestion.answers){
+
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    quizContainer.innerHTML = output.join('');
   }
-  {
-  question: "Who won the FIFA Women's World Cup in 2019?",
-  a: "No Longer Human",
-  b: "For the Tainted Sorrow",
-  c: "Rashomon",
-  d: "Unbreakable",
-  answer: "B"
+
+  function showResults(){
+
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    let numCorrect = 0;
+
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
-  {
-  question: "What is the smallest country in the world?",
-  a: "No Longer Human",
-  b: "For the Tainted Sorrow",
-  c: "Rashomon",
-  d: "Unbreakable",
-  answer: "C"
-  }
-  {
-  question: "Who invented the telephone?",
-  a: "No Longer Human",
-  b: "For the Tainted Sorrow",
-  c: "Rashomon",
-  d: "Unbreakable",
-  answer: "D"
-  }
-];
 
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint"
+      },
+      correctAnswer: "d"
+    }
+  ];
 
-function getRandomQuestion() {
-  var randomIndex = Math.floor(Math.random() * questions.length);
-  return questions[randomIndex];
-}
+  buildQuiz();
 
-function displayQuiz(maxQuestions) {
-  var quiz = document.getElementById("quiz");
-  var numQuestionsDisplayed = 0;
-
-
-  while (numQuestionsDisplayed < maxQuestions) {
-    var question = getRandomQuestion();
-
-    var questionDiv = document.createElement("div");
-    questionDiv.innerHTML = question;
-    quiz.appendChild(questionDiv);
-
-    numQuestionsDisplayed++;
-  }
-}
-
-displayQuiz(10);
-
+  submitButton.addEventListener('click', showResults);
+})();
