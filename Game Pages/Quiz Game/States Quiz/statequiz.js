ad0435daffbecb734f4834d894d0678e18971637
Anice Thompson <athompson186850@troy.edu>
2024-03-20 16:09:30 -0500

    Update statequiz.js

--- Game Pages/Quiz Game/States Quiz/statequiz.js
+++ Game Pages/Quiz Game/States Quiz/statequiz.js
@@ -690,7 +690,7 @@ function buildQuiz(){
 });
 
 returnButton.addEventListener('click', function() {
-    window.location.href = "../../quizgame.html";
+    window.location.href = "../../Quiz Game/quizgame.html";
 });
   
   previousButton.addEventListener("click", showPreviousSlide);
